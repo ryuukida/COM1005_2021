2b930b9d486a4c9b18c1549b1a0c06293372339b
Tony Stank <ewankambang@gmail.com>
2021-05-24 19:15:04 +0100

    Implemented abstract methods of SearchState class

--- search4/RamblersState.java
+++ search4/RamblersState.java
@@ -11,15 +11,32 @@ public class RamblersState extends SearchState{
     }
 
     public boolean goalPredicate(Search searcher) {
+        RamblersSearch rsearcher = (RamblersSearch)searcher;
+        RamblersState goalState = searcher.getGoalState();
+        return (goalState.xCoord.getx() == xCoord.getx() && goalState.gety() == yCoord.gety());
 
     }
 
     public ArrayList<SearchState> getSuccessors(Search searcher) {
-
+        RamblersSearch rsearcher = (RamblersSearch)searcher;
+        ArrayList<SearchState> sList = new ArrayList<>();
+        int x = xCoord.getx();
+        int y = yCoord.gety();
+        // Add successor states in horizontal directions
+        sList.add(new RamblersState(new Coords(x - 1, y)));
+        sList.add(new RamblersState(new Coords(x + 1, y)));
+        sList.add(new RamblersState(new Coords(x, y - 1)));
+        sList.add(new RamblersState(new Coords(x, y + 1)));
+        // Add successor states in vertical direction
+        sList.add(new RamblersState(new Coords(x - 1, y - 1)));
+        sList.add(new RamblersState(new Coords(x + 1, y + 1)));
+        sList.add(new RamblersState(new Coords(x + 1, y - 1)));
+        sList.add(new RamblersState(new Coords(x - 1, y + 1)));
     }
 
     public boolean sameState(SearchState s2) {
-        
+        RamblersState state1 = (RamblersState)s2;
+        return (state1.xCoord.getx() == xCoord.getx() && state1.xCoord.gety());
     }
 
 }
