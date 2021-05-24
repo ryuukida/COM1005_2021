cabdae47c7017d8f7e16c1c7afcc89e89e716756
Tony Stank <ewankambang@gmail.com>
2021-05-24 19:14:13 +0100

    Added terrain map to constructor

--- search4/RamblersSearch.java
+++ search4/RamblersSearch.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
 public class RamblersSearch extends Search {
+    private TerrainMap tmap;
+    private RamblersState initState;
+    private RamblersState goalState;
+
+    public TerrainMap getTmap() {
+        return tmap;
+    }
+
+    public RamblersState getGoalState() {
+        return goalState;
+    }
+
+    public RamblersSearch(TerrainMap tmap, RamblersState initState, RamblersState goalState) {
+        this.initState = initState;
+        this.goalState = goalState;
+        this.tmap = tmap;
+    }
 
 }
