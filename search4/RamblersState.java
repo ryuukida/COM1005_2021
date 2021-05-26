a77b10c7ece1e975c2163897dfdd1ba371af54af
Tony Stank <ewankambang@gmail.com>
2021-05-26 23:41:24 +0100

    Added functionality to getSuccessors method where local cost of state is stored. Also added helper function vetCoords to remove any successor coords that fall outside given tmap

--- search4/RamblersState.java
+++ search4/RamblersState.java
@@ -5,9 +5,10 @@ public class RamblersState extends SearchState{
     private int xCoord;
     private int yCoord;
 
-    public RamblersState(Coords initCoord) {
-        this.xCoord = initCoord.getx();
-        this.yCoord = initCoord.gety();
+    public RamblersState(Coords coord, int lc) {
+        this.xCoord = coord.getx();
+        this.yCoord = coord.gety();
+        super.localCost = lc;
     }
     public TerrainState(Coords coords) {
         xCoord = coords.getx();
@@ -23,19 +24,52 @@ public class RamblersState extends SearchState{
 
     public ArrayList<SearchState> getSuccessors(Search searcher) {
         RamblersSearch rsearcher = (RamblersSearch)searcher;
+        int [][] tmap = rsearcher.getTmap();
         ArrayList<SearchState> sList = new ArrayList<>();
         int x = xCoord.getx();
         int y = yCoord.gety();
-        // Add successor states in horizontal directions
-        sList.add(new RamblersState(new Coords(x - 1, y)));
-        sList.add(new RamblersState(new Coords(x + 1, y)));
-        sList.add(new RamblersState(new Coords(x, y - 1)));
-        sList.add(new RamblersState(new Coords(x, y + 1)));
-        // Add successor states in vertical direction
-        sList.add(new RamblersState(new Coords(x - 1, y - 1)));
-        sList.add(new RamblersState(new Coords(x + 1, y + 1)));
-        sList.add(new RamblersState(new Coords(x + 1, y - 1)));
-        sList.add(new RamblersState(new Coords(x - 1, y + 1)));
+        // Add all potential successor states relative to current state in
+        // array list
+        ArrayList<Coords> sCoords = new ArrayList<>();
+        // Horizontal
+        sCoords.add(new Coords(x - 1, y));
+        sCoords.add(new Coords(x + 1, y)); 
+        sCoords.add(new Coords(x, y - 1)); 
+        sCoords.add(new Coords(x, y + 1)); 
+        // Vertical
+        sCoords.add(new Coords(x - 1, y - 1)); 
+        sCoords.add(new Coords(x + 1, y + 1)); 
+        sCoords.add(new Coords(x + 1, y - 1)); 
+        sCoords.add(new Coords(x - 1, y + 1)); 
+        
+        // Remove any coords that do not fall within map
+        vetCoords(sCoords);
+
+        // Add all the successor states to sList
+        iterator<Coords> iterator = sCoords.iterator();
+        while (iterator.hasNext()) {
+            Coord coord = iterator.next();
+            sList.add(new RamblersState(coord, tmap[coord.getx()][coord.gety()]));  
+        }  
+    }
+
+    /**
+     * Helper method to return array list of succesor coords that are within the map
+     */
+    private ArrayList<Coords> vetCoords(ArrayList<Coords> sCoords) {
+        ListIterator<Coords> iterator = sCoords.listIterator();
+        while (iterator.hasNext()) {
+            int index = iterator.nextIndex();
+            Coord coord = iterator.next();
+            int sX = coord.getx();
+            int sY = coord.gety();
+            if ( (sX + 1 > tmap.width || sX - 1 < tmap.width) || (sY + 1 > tmap.height || sY - 1 < tmap.height) ) {
+                iterator.remove();
+            }
+            iterator.previous();
+        }
+
+        return sCoords;
     }
 
     public boolean sameState(SearchState s2) {
