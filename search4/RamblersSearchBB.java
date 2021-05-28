d85da9f7e934deb5cad7695175fd16936a1b6a47
Tony Stank <ewankambang@gmail.com>
2021-05-28 20:01:21 +0100

    Shows path to goal in new pgm file

--- search4/RamblersSearchBB.java
+++ search4/RamblersSearchBB.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class RamblersSearchBB {
 	public static void main(String [] args) {
 		int initX = 0;
@@ -11,6 +14,32 @@ public class RamblersSearchBB {
 		RamblersSearch searcher = new RamblersSearch(tmap, (RamblersState)goalState);
 
 		String res = searcher.runSearch(initState, "breadthFirst");
+		// Get list of nodes/coords that make up the path for the solution
+		SearchNode n = searcher.currentNode;
+		ArrayList<Coords> pathCoords = getCoordPathList(n);
+		// System.out.println("Printing path ");
+		// for (Coords c : pathCoords) {
+		// 	System.out.println(c);
+		// }
 		System.out.println(res);
+
+		tmap.showPath(pathCoords);
+		tmap.writeTMap("path.pgm");
+	}
+
+	private static ArrayList<Coords> getCoordPathList(SearchNode n) {
+		ArrayList<Coords> pathCoords = new ArrayList<>();
+		SearchNode currentNode = n;
+		while (currentNode.getParent() != null) {
+			RamblersState state = (RamblersState)currentNode.get_State();
+			pathCoords.add(state.getCoord());
+			currentNode = currentNode.getParent();
+			// Add final node to list
+			if (currentNode.getParent() == null) {
+				state = (RamblersState)currentNode.get_State();
+				pathCoords.add(state.getCoord());
+			}
+		}
+		return pathCoords;
 	}
 }
